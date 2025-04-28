65c3149072af6319745c69850b1884f721e24572
emilhilv <emilhilv@woodland.on.ca>
2025-04-28 12:13:20 -0400

    April 28

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ header = gui.Menu(root)
 header_account = gui.Menu(header, tearoff = 0)
 root.config(menu = header)
 header.add_cascade(label = "Account", menu = header_account)
-header_account.add_command(label = "View Messages", command = 'None')
+header_account.add_command(label = "View Employees", command = 'None')
 header_account.add_separator()
 header_account.add_command(label = "Sign Out...", command = 'None')
+header.add_cascade(label = "Stock")
 
-motto = Image.open("C:/Users/emilhilv/Downloads/wordart (4).png")
-motto = ImageTk.PhotoImage(motto)
-image_label = gui.Label(root, image=motto)
-image_label.pack()
+kanade = gui.PhotoImage(file = r'C:\Users\emilhilv\Downloads\lizarddog.png')
+gui.Button(root, text="please", image = kanade).pack()
+
+
+# motto = Image.open("C:/Users/emilhilv/Downloads/wordart (4).png")
+# motto = ImageTk.PhotoImage(motto)
+# image_label = gui.Label(root, image=motto)
+# image_label.pack()
 
 root.mainloop()
