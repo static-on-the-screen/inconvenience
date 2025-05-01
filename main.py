f2879a24320c3df6883a90f6573d9993a90e8cd9
static-on-the-screen <emilhilv@woodland.on.ca>
2025-05-01 11:45:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import tkinter as gui
-from PIL import Image, ImageTk
 
 root = gui.Tk()
 root.title("Inconvenience Store")
 root.geometry("900x650")
 root.configure(bg='#CAFAD5')
+
+
+def kanadewindow():
+    window = gui.Toplevel(root)
+    window.geometry("600x450")
+    window.configure(bg='#CAFAD5')
+    water = gui.PhotoImage(file = r'C:\Users\emilhilv\Pictures\dehydrated.png')
+    gui.Label(window, image=water).pack()
+    gui.Label(window, text="Dehydrated Water").pack()
+    gui.Label(window, text="NOTE: Item is permanently out of stock due to unwanted necessity.", fg="red").pack()
+    window.mainloop()
+
+
+def orbwindow():
+    root.destroy()
+    gui.Message()
 
 header = gui.Menu(root)
 header_account = gui.Menu(header, tearoff = 0)
@@ -15,9 +30,15 @@ header_account.add_separator()
 header_account.add_command(label = "Sign Out...", command = 'None')
 header.add_cascade(label = "Stock")
 
-kanade = gui.PhotoImage(file = r'C:\Users\emilhilv\Downloads\lizarddog.png')
-gui.Button(root, text="please", image = kanade).pack()
 
+kanade = gui.PhotoImage(file = r'C:\Users\emilhilv\Pictures\dehydrated.png')
+gui.Button(root, text="please", image = kanade, command = kanadewindow).pack()
+gui.Label(root, text="Dehydrated Water").pack()
+gnomesuke = gui.PhotoImage(file = r'C:\Users\emilhilv\Pictures\gnomesuke.png')
+gui.Button(root, text="please", image = gnomesuke, command = "None").pack()
+gui.Label(root, text="?????").pack()
+orb = gui.PhotoImage(file = r'C:\Users\emilhilv\Pictures\orb.png')
+gui.Button(root, image = orb, command = orbwindow).pack()
 
 # motto = Image.open("C:/Users/emilhilv/Downloads/wordart (4).png")
 # motto = ImageTk.PhotoImage(motto)
